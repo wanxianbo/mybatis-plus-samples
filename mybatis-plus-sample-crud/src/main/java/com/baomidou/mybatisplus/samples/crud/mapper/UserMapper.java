03a1c57f118513db12e5ab1928bd78eaf0cb1e94
wanxianbo <wanxb@jxgtmbjt.com>
2021-01-15 19:23:10 +0800

    update application.yml 中的spring.datasource.mysql

--- mybatis-plus-sample-crud/src/main/java/com/baomidou/mybatisplus/samples/crud/mapper/UserMapper.java
+++ mybatis-plus-sample-crud/src/main/java/com/baomidou/mybatisplus/samples/crud/mapper/UserMapper.java
@@ -1,4 +1,8 @@
 package com.baomidou.mybatisplus.samples.crud.mapper;
+import java.util.List;
+
+import com.baomidou.mybatisplus.samples.crud.vo.UserRoleVo;
+import org.apache.ibatis.annotations.Param;
 
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
@@ -13,5 +17,10 @@ import com.baomidou.mybatisplus.samples.crud.entity.User;
  * @since 2018-08-11
  */
 public interface UserMapper extends BaseMapper<User> {
+    User selectOneById(@Param("id") Long id);
+
+    List<User> selectByNameLike(@Param("likeName") String likeName);
+
+    UserRoleVo selectAllWithRoleById(@Param("id") Long id);
 
 }
