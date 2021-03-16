00144f09e7bb3c17f2798bfafa18201f784be095
wanxianbo <1036700294@qq.com>
2021-03-16 09:26:03 +0800

    Update MybatisPlusConfig.java
    add mybatis-plus 的动态表名的例子

--- mybatis-plus-sample-dynamic-tablename/src/main/java/com/baomidou/mybatisplus/samples/dytablename/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-dynamic-tablename/src/main/java/com/baomidou/mybatisplus/samples/dytablename/config/MybatisPlusConfig.java
@@ -7,6 +7,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -24,12 +25,8 @@ public class MybatisPlusConfig {
         DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor();
         HashMap<String, TableNameHandler> map = new HashMap<String, TableNameHandler>(2) {{
             put("user", (sql, tableName) -> {
-                String year = "_2018";
-                int random = new Random().nextInt(10);
-                if (random % 2 == 1) {
-                    year = "_2019";
-                }
-                return tableName + year;
+                int year = LocalDate.now().getYear();
+                return tableName + "_" + year;
             });
         }};
         dynamicTableNameInnerInterceptor.setTableNameHandlerMap(map);
